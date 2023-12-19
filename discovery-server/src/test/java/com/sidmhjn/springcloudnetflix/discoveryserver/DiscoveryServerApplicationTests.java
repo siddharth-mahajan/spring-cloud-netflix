25d30d9f0c1341d6f49d6871d89f167f14d204b7
siddharth-mahajan <siddharth.mhjn99@gmail.com>
2023-12-20 01:24:33 +0530

    initial commit

---
+++ discovery-server/src/test/java/com/sidmhjn/springcloudnetflix/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidmhjn.springcloudnetflix.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
